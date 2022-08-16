fda02d72d381edf5735bf0ccbdfaa29b5d660199
vishnu-punwani12 <vishnupunwani20@gmail.com>
2022-08-15 18:03:48 -0700

    students final grade

--- students-final-grade.js
+++ students-final-grade.js
@@ -1,21 +1,25 @@
-/* In this kata, your task is to implement an extended version of the famous rock-paper-scissors game. The rules are as follows:
+/* Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
 
-Scissors cuts Paper
-Paper covers Rock
-Rock crushes Lizard
-Lizard poisons Spock
-Spock smashes Scissors
-Scissors decapitates Lizard
-Lizard eats Paper
-Paper disproves Spock
-Spock vaporizes Rock
-Rock crushes Scissors
-Task:
-Given two values from the above game, return the Player result as "Player 1 Won!", "Player 2 Won!", or "Draw!".
+This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
 
-Inputs
-Values will be given as one of "rock", "paper", "scissors", "lizard", "spock".
+This function should return a number (final grade). There are four types of final grades:
 
+100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+0, in other cases
+Examples(Inputs-->Output):
+
+100, 12 --> 100
+99, 0 --> 100
+10, 15 --> 100
+
+85, 5 --> 90
+
+55, 3 --> 75
+
+55, 0 --> 0
+20, 2 --> 0
  */
 
 
@@ -23,64 +27,15 @@ Values will be given as one of "rock", "paper", "scissors", "lizard", "spock".
 
 /* MY SOLUTION */
 
-function rpsls(pl1,pl2){
- 
-    let player1 = pl1
-    let player2 = pl2
-    let draw = pl1 === pl2
-    
-    
-    if (player1 === "scissors" && player2 === "paper"){
-      return "Player 1 Won!" 
-    } else if (player1 === "paper" && player2 === "rock"){
-      return "Player 1 Won!"
-    } else if (player1 === "rock" && player2 === "lizard"){
-      return "Player 1 Won!"
-    } else if (player1 === "lizard" && player2 === "spock"){
-      return "Player 1 Won!"
-    } else if (player1 === "scissors" && player2 === "lizard"){
-      return "Player 1 Won!"
-    } else if (player1 === "lizard" && player2 === "paper"){
-      return "Player 1 Won!"
-    } else if (player1 === "paper" && player2 === "spock"){
-      return "Player 1 Won!"
-    } else if (player1 === "spock" && player2 === "rock"){
-      return "Player 1 Won!"
-    } else if (player1 === "rock" && player2 === "scissors"){
-      return "Player 1 Won!"
-    } else if (player1 === "spock" && player2 === "scissors"){
-      return "Player 1 Won!"
-    
-    } else if (player2 === "scissors" && player1 === "paper"){
-      return "Player 2 Won!"
-    } else if (player2 === "paper" && player1 === "rock"){
-      return "Player 2 Won!"
-    } else if (player2 === "rock" && player1 === "lizard"){
-      return "Player 2 Won!"
-    } else if (player2 === "lizard" && player1 === "spock"){
-      return "Player 2 Won!"
-    } else if (player2 === "scissors" && player1 === "lizard"){
-      return "Player 2 Won!"
-    } else if (player2 === "lizard" && player1 === "paper"){
-      return "Player 2 Won!"
-    } else if (player2 === "paper" && player1 === "spock"){
-      return "Player 2 Won!"
-    } else if (player2 === "spock" && player1 === "rock"){
-      return "Player 2 Won!"
-    } else if (player2 === "rock" && player1 === "scissors"){
-      return "Player 2 Won!"
-    } else if (player2 === "spock" && player1 === "scissors"){
-      return "Player 2 Won!"
-   
-    } else if (draw){
-      return "Draw!"
-   } else if (draw){
-      return "Draw!"
-   } else if (draw){
-      return "Draw!"
-   } else if (draw){
-      return "Draw!"
-    } else if (draw){
-      return "Draw!"
+function finalGrade (exam, projects) {
+  
+    if (exam > 90 || projects > 10){
+      return 100;
+    } else if (exam > 75 && projects >= 5){
+      return 90;
+    } else if (exam > 50 && projects >= 2){
+      return 75;
+    } else {
+      return 0;
     }
   }
